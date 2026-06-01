41e37a9172d659465980b61156eab4b073b51937
Brian Holwerda <brianholwerda@gmail.com>
2026-06-01 14:47:53 -0400

    Test Update to Warehouse.vue

--- customisations/bissell/extensions/RefundsDashboard/src/components/Warehouse.vue
+++ customisations/bissell/extensions/RefundsDashboard/src/components/Warehouse.vue
@@ -11,7 +11,7 @@
     <v-col cols="12">
       <v-card variant="outlined">
         <v-card-title class="BissellBanner">
-          Order Search
+          Order Search (test update)
         </v-card-title>
         <v-card-text class="pa-4">
           <v-text-field
