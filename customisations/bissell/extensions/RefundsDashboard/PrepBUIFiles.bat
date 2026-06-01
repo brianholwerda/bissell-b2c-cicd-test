16dac5b42afbc506e128d13171e6eb8db9860942
Brian Holwerda <brianholwerda@gmail.com>
2026-06-01 13:25:17 -0400

    Make PrepBUIFiles.bat use relative paths

--- customisations/bissell/extensions/RefundsDashboard/PrepBUIFiles.bat
+++ customisations/bissell/extensions/RefundsDashboard/PrepBUIFiles.bat
@@ -1,6 +1,6 @@
 copy initialize.html RefundsDashboard
 copy configuration.json RefundsDashboard
 
-powershell -command "(Get-Content 'D:\OSvC\BUI\RefundsDashboard\RefundsDashboard\index.html') -replace '/RefundsDashboard','./RefundsDashboard' | Set-Content 'D:\OSvC\BUI\RefundsDashboard\RefundsDashboard\index.html'"
+powershell -command "(Get-Content '.\RefundsDashboard\index.html') -replace '/RefundsDashboard','./RefundsDashboard' | Set-Content '.\RefundsDashboard\index.html'"
 
-powershell -command "(Get-Content 'D:\OSvC\BUI\RefundsDashboard\RefundsDashboard\RefundsDashboard5.css') -replace '/RefundsDashboard','./RefundsDashboard' | Set-Content 'D:\OSvC\BUI\RefundsDashboard\RefundsDashboard\RefundsDashboard5.css'"
+powershell -command "(Get-Content '.\RefundsDashboard\RefundsDashboard5.css') -replace '/RefundsDashboard','./RefundsDashboard' | Set-Content '.\RefundsDashboard\RefundsDashboard5.css'"
